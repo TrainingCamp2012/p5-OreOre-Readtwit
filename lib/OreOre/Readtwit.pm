7c574037352ff63fb8ae5c8aab0af5a961a3bab0
Tor Ozaki <tor.ozaki@gmail.com>
2012-02-21 23:44:57 +0900

    init with Module::Setup

---
+++ lib/OreOre/Readtwit.pm
@@ -0,0 +1,32 @@
+package OreOre::Readtwit;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+OreOre::Readtwit -
+
+=head1 SYNOPSIS
+
+  use OreOre::Readtwit;
+
+=head1 DESCRIPTION
+
+OreOre::Readtwit is
+
+=head1 AUTHOR
+
+Tor Ozaki E<lt>tor.ozaki@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
